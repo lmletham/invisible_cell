232d87dfd7ae9da139d49e87dc698422e0f0558c
lmletham <lydia@csail.mit.edu>
2024-08-10 12:25:30 -0600

    pulling elixir back one

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule InvisibleCell.MixProject do
     [
       app: :invisible_cell,
       version: "0.1.0",
-      elixir: "~> 1.17.2",
+      elixir: "~> 1.17.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
